a66740e176126168fb6139eee135490355b5e1e5
Alex Efremenkov <bazooka2008@inbox.ru>
2025-07-11 17:07:30 +0300

    sss

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,9 @@
 import 'package:bc_phthalmoscopy/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-// class MyHttpOverrides extends HttpOverrides {
-//   @override
-//   HttpClient createHttpClient(SecurityContext? context) {
-//     return super.createHttpClient(context)
-//       ..badCertificateCallback =
-//           (X509Certificate cert, String host, int port) => true;
-//   }
-// }
 
 void main() async {
-  await dotenv.load(fileName: ".env.univer");
-  // HttpOverrides.global = MyHttpOverrides();
-  // await FlutterSecureStorage().deleteAll();
+  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
